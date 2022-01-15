f07b0f12073f80495156016c8dedddbd0c2eea66
Martin Weiss <xweiss3@mendelu.cz>
2022-01-15 22:50:37 +0100

    Added logo

--- components/Home/FilterForm.js
+++ components/Home/FilterForm.js
@@ -37,17 +37,17 @@ const FilterForm = () => {
             <div className="flex gap-2">
 				<div className="w-72">
 					<div className="relative rounded-md shadow-sm">
-						<input type="text" className="block w-full pl-3 pr-32 sm:text-sm rounded-md focus:ring-indigo-500 focus:border-indigo-500 border-gray-300" placeholder="superdoména" onChange={e => setdomainName(e.target.value)}/>
+						<input type="text" className="block w-full pl-3 pr-32 sm:text-sm rounded-md focus:ring-emerald-500 focus:border-emerald-500 border-gray-300" placeholder="superdoména" onChange={e => setdomainName(e.target.value)}/>
 						<div className="absolute inset-y-0 right-0 flex items-center">
 							<label htmlFor="currency" className="sr-only">TLD</label>
-							<input id="tld-choice" name="tld-choice" list="tlds" placeholder=".cz" className="w-32 h-full py-0 pl-2 pr-1 sm:text-sm rounded-md focus:ring-indigo-500 focus:border-indigo-500 border-transparent bg-transparent text-gray-500" onChange={e => setselectedTLD(e.target.value)}/>
+							<input id="tld-choice" name="tld-choice" list="tlds" placeholder=".cz" className="w-32 h-full py-0 pl-2 pr-1 sm:text-sm rounded-md focus:ring-emerald-500 focus:border-emerald-500 border-transparent bg-transparent text-gray-500" onChange={e => setselectedTLD(e.target.value)}/>
 							<datalist id="tlds">
 								{TLDList.map((tld, tldKey) => <option key={tldKey} value={tld}/>)}
 							</datalist>
 						</div>
 					</div>
 				</div>
-				<button type="submit" className="flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-gray-200" disabled={checkButtonState} onClick={domainDataLoader}>Ověřit</button>
+				<button type="submit" className="flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:bg-gray-200" disabled={checkButtonState} onClick={domainDataLoader}>Ověřit</button>
 			</div>
         </>
     )
