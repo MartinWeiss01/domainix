52a5e891af65d376c371f04098ce58e18886d7c9
Martin Weiss <xweiss3@mendelu.cz>
2022-01-17 13:10:50 +0100

    Added links

--- libs/registrarsList.js
+++ libs/registrarsList.js
@@ -1,10 +1,10 @@
 const registrarsList = {
-    Wedos: {img: "/registrars/resized/wedos.png"},
-    Subreg: {img: "/registrars/resized/subreg.png"},
-    Forpsi: {img: "/registrars/forpsi.svg"},
-    Pipni: {img: "/registrars/resized/pipni.png"},
-    Endora: {img: "/registrars/resized/endora.png"},
-    Websupport: {img: "/registrars/websupport.svg"},
+    Wedos: {link: "https://www.wedos.cz/", img: "/registrars/resized/wedos.png"},
+    Subreg: {link: "https://subreg.cz/cz/", img: "/registrars/resized/subreg.png"},
+    Forpsi: {link: "https://www.forpsi.com/", img: "/registrars/forpsi.svg"},
+    Pipni: {link: "https://pipni.cz/", img: "/registrars/resized/pipni.png"},
+    Endora: {link: "https://www.endora.cz/", img: "/registrars/resized/endora.png"},
+    Websupport: {link: "https://www.websupport.cz/", img: "/registrars/websupport.svg"},
 }
 
 module.exports = {
