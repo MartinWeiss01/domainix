dad36cead679dd52cd7595ef85417dcdfbf9eb07
Martin Weiss <xweiss3@mendelu.cz>
2022-01-17 16:32:17 +0100

    Changed button text

--- components/Home/FilterForm.js
+++ components/Home/FilterForm.js
@@ -47,7 +47,7 @@ const FilterForm = () => {
 						</div>
 					</div>
 				</div>
-				<button type="submit" className="flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:bg-gray-200" disabled={checkButtonState} onClick={domainDataLoader}>Ověřit</button>
+				<button type="submit" className="flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 disabled:bg-gray-200" disabled={checkButtonState} onClick={domainDataLoader}>Zjistit ceny</button>
 			</div>
         </>
     )
