b64e7f228b5e5a661f93ba673f2a260b21b705d0
Martin Weiss <xweiss3@mendelu.cz>
2022-01-19 15:37:43 +0100

    Registrars Component moved directly to its page

--- pages/registrars/index.js
+++ pages/registrars/index.js
@@ -1,10 +1,22 @@
-import Registrars from "../../components/Registrars"
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import { registrarsList } from "../../libs/registrarsList";
 
 const RegistrarsPage = () => {
+    const data = useSelector((state) => state.mainer.data)
+
     return (
-        <>
-            <Registrars />
-        </>
+        <div className="flex gap-10 flex-wrap">
+            {data.map((registrator, registratorKey) => 
+                registrarsList[registrator.name] ? (
+                    <Link key={registratorKey} href={`/registrars/${registrator.slug}`}>
+                        <a className="flex justify-center items-center w-40 h-20 opacity-50 hover:opacity-100 transition-opacity">
+                            <img className="max-h-full" src={registrarsList[registrator.name].img} />
+                        </a>
+                    </Link>
+                ) : (<></>)
+            )}
+        </div>
     )
 }
 
