803016a2fb60e2dd34bc2e5bc31130dfd786a81e
Martin Weiss <xweiss3@mendelu.cz>
2022-01-19 16:32:03 +0100

    Replaced flex with grid

--- pages/registrars/index.js
+++ pages/registrars/index.js
@@ -6,17 +6,22 @@ const RegistrarsPage = () => {
     const data = useSelector((state) => state.mainer.data)
 
     return (
-        <div className="flex gap-10 flex-wrap">
-            {data.map((registrator, registratorKey) => 
-                registrarsList[registrator.name] ? (
-                    <Link key={registratorKey} href={`/registrars/${registrator.slug}`}>
-                        <a className="flex justify-center items-center w-40 h-20 opacity-50 hover:opacity-100 transition-opacity">
-                            <img className="max-h-full" src={registrarsList[registrator.name].img} />
-                        </a>
-                    </Link>
-                ) : (<></>)
-            )}
-        </div>
+        <>
+            <p className="text-gray-400 text-sm mb-6">Pro zobrazení podrobností zvol libovolného registrátora z nabídky:</p>
+            <div className="grid grid-cols-2 gap-y-2 sm:grid-cols-3 md:grid-cols-2 md:gap-y-8 lg:grid-cols-4 xl:grid-cols-5 gap-x-4">
+                {data.map((registrator, registratorKey) => 
+                    registrarsList[registrator.name] ? (
+                        <div className="flex justify-center items-center">
+                            <Link key={registratorKey} href={`/registrars/${registrator.slug}`}>
+                                <a className="flex justify-center items-center w-40 h-20 opacity-50 hover:opacity-100 transition-opacity">
+                                    <img className="max-h-full" src={registrarsList[registrator.name].img} />
+                                </a>
+                            </Link>
+                        </div>
+                    ) : (<></>)
+                    )}
+            </div>
+        </>
     )
 }
 
