555c1ee0363f7afdb04b122615e71233d86bb008
Martin Weiss <xweiss3@mendelu.cz>
2022-01-19 15:23:05 +0100

    Changed slug of registrar's page

--- components/Registrars.js
+++ components/Registrars.js
@@ -6,17 +6,17 @@ const Registrars = () => {
     const data = useSelector((state) => state.mainer.data)
 
     return (
-        <>
-            <div className="flex gap-10 flex-wrap">
-                {data.map((registrator, registratorKey) => (
-                    <Link key={registratorKey} href={`/registrars/${registratorKey}`}>
+        <div className="flex gap-10 flex-wrap">
+            {data.map((registrator, registratorKey) => 
+                registrarsList[registrator.name] ? (
+                    <Link key={registratorKey} href={`/registrars/${registrator.slug}`}>
                         <a className="flex justify-center items-center w-40 h-20 opacity-50 hover:opacity-100 transition-opacity">
-                            <img className="max-h-full" src={registrarsList[registrator.name]?.img || "https://via.placeholder.com/256x128"} />
+                            <img className="max-h-full" src={registrarsList[registrator.name].img} />
                         </a>
                     </Link>
-                ))}
-            </div>
-        </>
+                ) : (<></>)
+            )}
+        </div>
     )
 }
 
--- pages/registrars/[id].js
+++ pages/registrars/[id].js
@@ -4,26 +4,35 @@ import { useSelector } from "react-redux";
 import { registrarsList } from "../../libs/registrarsList";
 import { ExternalLinkIcon } from "@heroicons/react/outline"
 
+const getRegistrarIDBySlug = (data, slug) => {
+    for(let i = 0; i < data.length; i++) {
+        if(data[i].slug === slug) {
+            return i
+        }
+    }
+}
+
 const RegistrarDetail = () => {
     const router = useRouter()
-    const { id } = router.query
     const data = useSelector((state) => state.mainer.data)
     const taxes = useSelector((state) => state.mainer.taxes)
+    const { id } = router.query
+    const regIndex = getRegistrarIDBySlug(data, id)
 
-    if(data[id] !== undefined) {
+    if(data[regIndex] !== undefined) {
         return (
             <>
                 <div>
                     <div className="bg-white shadow-xl rounded-lg overflow-hidden mb-8 max-w-md">
                         <div className="bg-cover bg-center h-32 p-4 flex justify-center items-center" >
-                            <img className="max-h-full" src={registrarsList[data[id].name]?.img || "https://via.placeholder.com/256x128"} />
+                            <img className="max-h-full" src={registrarsList[data[regIndex].name]?.img || "https://via.placeholder.com/256x128"} />
                         </div>
 
                         <div className="px-4 pb-3 pt-4 bg-gray-100 flex justify-between flex-wrap">
                         <div className="text-xs uppercase font-bold text-gray-600 tracking-wide">
-                            {data[id].name}
+                            {data[regIndex].name}
                         </div>
-                        <Link href={registrarsList[data[id].name]?.link || "/"}>
+                        <Link href={registrarsList[data[regIndex].name]?.link || "/"}>
                             <a target="_blank">
 						        <div className="flex text-xs uppercase font-bold text-gray-600 tracking-wide items-center">Web <ExternalLinkIcon className="h-4 w-4" /></div>
                             </a>
@@ -32,10 +41,10 @@ const RegistrarDetail = () => {
 
                         <div className="flex justify-between items-center p-4 border-t border-gray-300 text-gray-600 flex-wrap">
                             <div className="flex items-center">
-                                <p><span className="text-sm pr-1">Počet TLDs</span> <span className="text-gray-900 font-bold">{data[id].domains.length}</span></p>
+                                <p><span className="text-sm pr-1">Počet TLDs</span> <span className="text-gray-900 font-bold">{data[regIndex].domains.length}</span></p>
                             </div>
                             <div className="flex items-center">
-                                <p><span className="text-sm pr-1">Aktualizováno</span> <span className="text-gray-900 font-bold">{data[id].updated}</span></p>
+                                <p><span className="text-sm pr-1">Aktualizováno</span> <span className="text-gray-900 font-bold">{data[regIndex].updated}</span></p>
                             </div>
                         </div>
                     </div>
@@ -51,7 +60,7 @@ const RegistrarDetail = () => {
                                 </tr>
                             </thead>
                             <tbody className="bg-white divide-y divide-gray-200">
-                                {data[id].domains.map((row, rowKey) => (
+                                {data[regIndex].domains.map((row, rowKey) => (
                                     <tr key={rowKey}>
                                     <td className="px-6 py-4 whitespace-nowrap">
                                         <div className="flex items-center">
