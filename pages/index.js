d97229cf0118fb188032ac4fd1b9b7c590303aff
Martin Weiss <xweiss3@mendelu.cz>
2022-01-19 16:00:59 +0100

    Moved data fetching into Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,13 @@
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { setData } from "../app/mainSlice"
 import ComparedTable from "../components/Home/ComparedTable";
 import FilterForm from "../components/Home/FilterForm";
 
-const Home = () => {
-	const [ready, setReady] = useState(false);
-	const dispatch = useDispatch();
-
-	useEffect(() => {
-		fetch('https://srv.martinweiss.cz/api/v1/domainix')
-		.then(response => {
-		  if(response.ok) return response.json();
-		  return Promise.reject(response);
-		})
-		.then(data => {
-			dispatch(setData(data))
-			setReady(true)
-		})
-		.catch(err => console.error(`[!] An error occurred while processing the script.`));
-	}, [])
-
-	if(ready) {
-		return (
-			<div>	
-				<FilterForm />
-				<ComparedTable />
-			</div>
-		)
-	} else {
-		return (
-			<span>Načítání...</span>
-		)
-	}
-	
+const Dashboard = () => {
+	return (
+		<div>	
+			<FilterForm />
+			<ComparedTable />
+		</div>
+	)
 }
 
-export default Home
+export default Dashboard
